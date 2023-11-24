d2ca413669acbf1605c6de7b0665ad65e6701a06
Chris Maissan <chris@maissan.net>
2023-11-23 22:45:31 -0500

    Roughed in front end styles

--- value-prop/assets/js/value-prop.js
+++ value-prop/assets/js/value-prop.js
@@ -1,9 +1,42 @@
 (function() {
 
+    let containerEl = document.querySelector('.value-prop');
     let inputEl = document.querySelector('.value-prop__input');
     let submitEl = document.querySelector('.value-prop__submit');
+    let responseEl = document.querySelector('.value-prop__response');
+    let errorEl = document.querySelector('.value-prop__error');
 
+    /**
+     * Display response.
+     * 
+     * @param {string} response 
+     */
+    function displayResponse(response) {
+        let clean = response.replace(/(<([^>]+)>)/gi, '');
+        let paragraphs = clean.split('\n');
+        let html = '';
+        for (let i = 0; i < paragraphs.length; i = i + 1) {
+            html += '<p>' + paragraphs[i] + '</p>'
+        }
+        errorEl.style.display = 'none';
+        responseEl.innerHTML = html;
+    }
+
+    /**
+     * Display error message..
+     * 
+     * @param {string} error 
+     */
+    function displayError(error) {
+        errorEl.style.display = 'block';
+        errorEl.innerHTML = error;
+    }
+
+    /**
+     * Make API request when form submitted.
+     */
     submitEl.addEventListener('click', () => {
+        containerEl.classList.toggle('loading', true);
         fetch('https://cutejudiciousdeveloper.jsutton.repl.co/vp', {
             method: 'POST',
             mode: 'cors',
@@ -16,8 +49,14 @@
         }).then((response) => {
             return response.json()
         }).then((data) => {
-            console.log(data);
-        });
+            if (!data.choices || data.choices.length === 0) {
+                displayError('Invalid response');
+            } else {
+                displayResponse(data.choices[0].message.content);
+            }
+        }).finally(() => {
+            containerEl.classList.toggle('loading', false);
+        })
     });
 
 })();
--- value-prop/widgets/value-prop.php
+++ value-prop/widgets/value-prop.php
@@ -4,12 +4,20 @@ class Value_Prop_Widget extends \Elementor\Widget_Base {
 
     public function __construct($data = [], $args = null) {
         parent::__construct($data, $args);
+
         wp_register_script( 
             'value-prop', 
             plugin_dir_url( dirname( __FILE__ ) ) . '/assets/js/value-prop.js', 
             [ 'elementor-frontend' ], 
             '1.0.0',
              true 
+        );
+
+        wp_register_style( 
+            'value-prop', 
+            plugin_dir_url( dirname( __FILE__ ) ) . '/assets/css/value-prop.css', 
+            [ 'elementor-frontend' ], 
+            '1.0.0'
         );
      }
 
@@ -37,16 +45,37 @@ class Value_Prop_Widget extends \Elementor\Widget_Base {
         return [ 'value-prop' ];
     }
 
+    public function get_style_depends() {
+		return [ 'value-prop' ];
+	}
+
 	protected function render() {
         ?>
             <div class="value-prop">
-                <h1>What's my <em>Value Proposition?</em></h1>
+                <h2 class="value-prop__headline">What's my <em>Value Proposition?</em></h2>
                 <p class="value-prop__instructions">
                     Enter the address of any business' website to see how OpenAI interprets its Value Proposition to be.
                 </p>
                 <div class="value-prop__input-group">
                     <input class="value-prop__input" placeholder="https://" autofocus>
                     <button class="value-prop__submit">Submit</button>
+                </div>
+                <div class="value-prop__error"></div>
+                <div class="value-prop__response"></div>
+                <div class="value-prop__footer">
+                    <p>
+                        This product is still in under development and may at times not be fully functional. We are 
+                        managing daily, the number of concurrent requests to evaluate output scalability. We apologize 
+                        for any inconvenience and appreciate your patience as we work to scale and expand the product.
+                    </p>
+                </div>
+                <div class="value-prop__spinner">
+                    <div class="value-prop__spinner__inner">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </div>
             </div>
         <?php
